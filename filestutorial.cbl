f0f0b22be9cef2485c16d3354a9b8ecd876bed5b
Diego <Diego Gonzlez>
2019-01-07 12:54:40 -0300

    changes

--- filestutorial.cbl
+++ filestutorial.cbl
@@ -17,18 +17,25 @@
            FILE SECTION.
            FD STUDENT.
            01 STUDENT-FILE.
-               05 STUDENT-ID PIC 9(4).
-               05 STUDENT-NAME PIC A(5).
+               05 STUDENT-ID PIC 9(5).
+               05 STUDENT-NAME PIC A(6).
                05 STUDENT-MARK PIC 9(3).
 
            WORKING-STORAGE SECTION.
            01 WS-STUDENT.
-               05 WS-STUDENT-ID PIC 9(4).
-               05 WS-STUDENT-NAME PIC A(5).
+               05 WS-STUDENT-ID PIC 9(5).
+               05 WS-STUDENT-NAME PIC A(6).
                05 WS-STUDENT-MARK PIC 9(3).
 
            01 WS-EOF PIC A(1).
+
+           01 WS-TABLE.
+               05 WS-ID PIC A(5) VALUE "ID".
+               05 WS-NAME PIC A(6) VALUE "NAME".
+               05 WS-MARK PIC A(4) VALUE "MARK".
+
        PROCEDURE DIVISION.
+           DISPLAY WS-TABLE
             OPEN INPUT STUDENT.
                PERFORM UNTIL WS-EOF = 'Y'
                    READ STUDENT INTO WS-STUDENT
